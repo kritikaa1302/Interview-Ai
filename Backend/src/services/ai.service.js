7b6176d257208d2907d4247b1504a416fdc2a5b3
kritikaa1302 <kritikagrover1302@gmail.com>
2026-09-25 23:13:35 +0530

    Update ai.service.js

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -6,8 +6,6 @@ const puppeteer = require("puppeteer")
 const ai = new GoogleGenAI({
     apiKey: process.env.GOOGLE_GENAI_API_KEY
 })
-
-
 const interviewReportSchema = z.object({
     matchScore: z.number().describe("A score between 0 and 100 indicating how well the candidate's profile matches the job describe"),
     technicalQuestions: z.array(z.object({
